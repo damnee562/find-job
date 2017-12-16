ba80e1c6af2e19d524a7bd35a650884cbe9ef501
damnee562 <damnee562@gmail.com>
2017-12-16 14:10:44 +0800

    Crawl jobs from inside jobs.

---
+++ inside_jobs/inside_jobs/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class InsideJobsItem(scrapy.Item):
+    company = scrapy.Field()
+    name = scrapy.Field()
+    category = scrapy.Field()
+    location = scrapy.Field()
+    salary = scrapy.Field()
+    url = scrapy.Field()
---
+++ inside_jobs/inside_jobs/spiders/jobs_spider.py
@@ -0,0 +1,34 @@
+import scrapy
+
+from inside_jobs.items import InsideJobsItem
+
+class JobsSpider(scrapy.Spider):
+    name = 'jobs'
+
+    def start_requests(self):
+        url = 'https://jobs.inside.com.tw'
+        keyword = getattr(self, 'keyword', None)
+        if keyword is not None:
+            url = 'https://jobs.inside.com.tw/jobs/index?k={}'.format(keyword)
+
+        yield scrapy.Request(url, self.parse)
+
+    def parse(self, response):
+        for href in response.css('div.list-group a.list-group-item::attr(href)'):
+            yield response.follow(href, self.parse_details)
+
+        next_page = response.css('ul.pagination li')[-1].css('a::attr(href)').extract_first()
+
+        if next_page is not None and next_page != '/jobs/index?k={}'.format(self.keyword):
+            yield response.follow(next_page, self.parse)
+
+    def parse_details(self, response):
+        item = InsideJobsItem()
+        item['company'] = response.css('div.col-sm-9 div.panel-heading h1::text').extract_first().strip()
+        item['name'] = response.css('div.col-sm-8 h1::text').extract_first().strip()
+        item['category'] = response.css('div.col-sm-8 p::text').extract_first().split('：')[1].strip()
+        item['location'] = response.css('div.col-sm-8 p::text')[1].extract().split('：')[1].strip()
+        item['salary'] = response.css('div.col-sm-8 p::text')[2].extract().split('：')[1].strip()
+        item['url'] = response.url
+
+        yield item
